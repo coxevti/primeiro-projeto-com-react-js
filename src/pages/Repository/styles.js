f9738150bb400631c0650a2a7dcc5809a611958c
Valdir Coxev <valdircoxevdev@gmail.com>
2020-01-12 15:45:06 -0400

    Exibindo issues

--- src/pages/Repository/styles.js
+++ src/pages/Repository/styles.js
@@ -39,3 +39,57 @@ export const Owner = styled.header`
         max-width: 400px;
     }
 `;
+
+export const IssueList = styled.ul`
+    padding-top: 30px;
+    margin-top: 30px;
+    border-top: 1px solid #eee;
+    list-style: none;
+    li {
+        display: flex;
+        padding: 15px 10px;
+        border: 1px solid #eee;
+        border-radius: 4px;
+        & + li {
+            margin-top: 10px;
+        }
+        img {
+            width: 48px;
+            height: 48px;
+            border-radius: 50%;
+            border: 2px solid #eee;
+        }
+        div {
+            display: flex;
+            flex-direction: column;
+            flex: 1;
+            margin-left: 16px;
+            justify-content: center;
+            strong {
+                font-size: 16px;
+                a {
+                    text-decoration: none;
+                    color: #333;
+                    &:hover {
+                        color: #7159c1;
+                    }
+                }
+                span {
+                    font-size: 12px;
+                    font-weight: 600;
+                    margin-left: 10px;
+                    padding: 3px 4px;
+                    height: 20px;
+                    background: #eee;
+                    border-radius: 2px;
+                    color: #333;
+                }
+            }
+            p {
+                margin-top: 5px;
+                font-size: 12px;
+                color: #999;
+            }
+        }
+    }
+`;
